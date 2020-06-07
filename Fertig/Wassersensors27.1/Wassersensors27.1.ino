26fcf4223c5e71433b9f1400ae5f001ea38a7488
id3vi5er <63673957+id3vi5er@users.noreply.github.com>
2020-06-07 23:48:52 +0200

    Add files via upload

--- Fertig/Wassersensors27.1/Wassersensors27.1.ino
+++ Fertig/Wassersensors27.1/Wassersensors27.1.ino
@@ -19,26 +19,6 @@ const char* mqtt_server = "192.168.0.10";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-/*void print_wakeup_touchpad(){
-  touchPin = esp_sleep_get_touchpad_wakeup_status();
-
-  switch(touchPin)
-  {
-    case 0  : Serial.println("Touch detected on GPIO 4"); break;
-    case 1  : Serial.println("Touch detected on GPIO 0"); break;
-    case 2  : Serial.println("Touch detected on GPIO 2"); break;
-    case 3  : Serial.println("Touch detected on GPIO 15"); break;
-    case 4  : Serial.println("Touch detected on GPIO 13"); break;
-    case 5  : Serial.println("Touch detected on GPIO 12"); break;
-    case 6  : Serial.println("Touch detected on GPIO 14"); break;
-    case 7  : Serial.println("Touch detected on GPIO 27"); break;
-    case 8  : Serial.println("Touch detected on GPIO 33"); break;
-    case 9  : Serial.println("Touch detected on GPIO 32"); break;
-    default : Serial.println("Wakeup not by touchpad"); break;
-  }
-}
-*/
-
 void callbackAlarm(){
 }
 
@@ -47,9 +27,7 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -78,9 +56,9 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
-    String clientId = "Wassersensor1";
-    //clientId += String(random(0xffff), HEX);
-    // Attempt to connect
+    String clientId = "Wassersensor1-";
+    clientId += String(random(0xffff), HEX);
+    //Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       } else {
@@ -96,12 +74,8 @@ void reconnect() {
 void setup(){
   delay(750);
   Serial.begin(115200);
-  delay(200);
-  ++bootCount;
-  setup_wifi();
   delay(500);
-
-  delay(150);
+  setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   
@@ -110,23 +84,24 @@ void setup(){
   Serial.println("MQTT Verbunden!");
   client.loop();
   
-  char bootString[50];
-  dtostrf(bootCount, 50, 1, bootString);
   
-  
-  client.publish("/alarm/Wassersensors1/", bootString);
+  if (bootCount >= 1){
+  client.publish("/alarm/Wassersensor1/", "Wasser");
   delay(200);
-  Serial.println("MQTT Gesendet!");
+  Serial.print("MQTT Gesendet: ");
+  Serial.println("Alarm");
   Serial.println(bootCount);
-  /* 
-  //Print the wakeup reason for ESP32 and touchpad too
-  print_wakeup_reason();
-  print_wakeup_touchpad();
-  */
-  
-  //Setup interrupt on Touch Pad 3 (GPIO15)
-  touchAttachInterrupt(T3, callbackAlarm, Threshold);
-  //Serial.println(bootCount);
+  } else {
+    client.publish("/alarm/Wassersensor1", "OK");
+    delay(200);
+    Serial.print("MQTT Gesendet: ");
+    Serial.println("OK!");
+    Serial.println(bootCount);
+    
+    }
+
+  touchAttachInterrupt(T3, callbackAlarm, Threshold); //Setup interrupt on Touch Pad 3 (GPIO15)
+  ++bootCount;
   esp_sleep_enable_touchpad_wakeup();
   Serial.println("ESP goes to Deepsleep");
   esp_deep_sleep_start();
@@ -135,3 +110,38 @@ void setup(){
 void loop(){
   //This will never be reached
 }
+
+
+
+/*
+
+
+  char bootString[50];
+  dtostrf(bootCount, 50, 1, bootString);
+
+
+  
+void print_wakeup_touchpad(){
+  touchPin = esp_sleep_get_touchpad_wakeup_status();
+
+  switch(touchPin)
+  {
+    case 0  : Serial.println("Touch detected on GPIO 4"); break;
+    case 1  : Serial.println("Touch detected on GPIO 0"); break;
+    case 2  : Serial.println("Touch detected on GPIO 2"); break;
+    case 3  : Serial.println("Touch detected on GPIO 15"); break;
+    case 4  : Serial.println("Touch detected on GPIO 13"); break;
+    case 5  : Serial.println("Touch detected on GPIO 12"); break;
+    case 6  : Serial.println("Touch detected on GPIO 14"); break;
+    case 7  : Serial.println("Touch detected on GPIO 27"); break;
+    case 8  : Serial.println("Touch detected on GPIO 33"); break;
+    case 9  : Serial.println("Touch detected on GPIO 32"); break;
+    default : Serial.println("Wakeup not by touchpad"); break;
+  }
+}
+
+  //Print the wakeup reason for ESP32 and touchpad too
+  print_wakeup_reason();
+  print_wakeup_touchpad();
+
+*/
